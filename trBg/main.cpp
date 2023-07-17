5f937c8f2ebc0910dcfd5db64fb365a799934200
M-Farag <mina.farag@icloud.com>
2023-07-17 23:46:08 +0300

    Initial Commit

---
+++ trBg/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  trBg
+//
+//  Created by Mina on 17/07/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
